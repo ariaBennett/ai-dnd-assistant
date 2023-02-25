7f99b645b0fe67237c4866bbacf46d6c042e2db4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2023-02-25 15:37:16 +0100

    update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,7 @@
+_CODE_HIGH_MODEL = "code-davinci-002"
+_CODE_LOW_MODEL = "code-cushman-001"
+CODE_CONFIG = {"model": _CODE_LOW_MODEL, "max_tokens": 2048}
+
 TEXT_CONFIG = {"model": "text-ada-001", "max_tokens": 128, "temperature": 1}
 
 _SMALL = "256x256"
@@ -6,3 +10,6 @@ _LARGE = "1024x1024"
 IMAGE_CONFIG = {"response_format": "b64_json", "size": _SMALL}
 
 ART_STYLE = "D&D dark fantasy handbook style in color painting"
+
+with open("app/schemas/monster.json") as f_in:
+    MONSTER_SCHEMA = f_in.read()
